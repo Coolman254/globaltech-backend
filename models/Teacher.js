f758eae424b1fd0e325d3db2cd99021ee55e7d70
joseph <josephwainaina053@gmail.com>
2026-04-05 23:45:35 +0300

    fix: teacherId to string

--- models/Teacher.js
+++ models/Teacher.js
@@ -8,7 +8,7 @@ const teacherSchema = new mongoose.Schema(
     age:             { type: Number, required: true },
     email:           { type: String, required: true, unique: true, lowercase: true, trim: true },
     phone:           { type: String, default: null },
-    teacherId:       { type: Number, required: true, unique: true },
+    teacherId:       { type: String, required: true, unique: true },
     subject:         { type: String, required: true },
     salaryKsh:       { type: Number, required: true },
     employmentType:  { type: String, enum: ["fulltime", "parttime"], default: "fulltime" },
